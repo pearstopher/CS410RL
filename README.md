a1e4b6a7a2fad1c70e6667cf13a1987ce7d529f6
Pearstopher <pearstopher@gmail.com>
2022-09-27 15:42:43 -0700

    added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# CS41O Reinforcement Learning
+
+Each individual project/assignment is included in its own separate branch.
--- main.py
+++
@@ -1,9 +0,0 @@
-# Christopher Juncker
-# CS410 Reinforcement Learning Fall 2022
-# Term Project
-#
-#
-
-if __name__ == '__main__':
-    print("CS410 Reinforcement Learning Project")
-
