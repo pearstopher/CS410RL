1b3e52a9cf2c8ae7d89a78728517fd6197e4a36e
Pearstopher <pearstopher@gmail.com>
2022-09-29 14:47:06 -0700

    added world abilities: left, right, forward

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Environment:
         # set the location of the destination (single gathering node)
         self.node = self.random_location()
         # set the location where agents will begin when entering this world
-        self.home = self.random_location()
+        self.agent = self.random_location()
         # and the direction the agent will be facing when they "spawn"
         self.orientation = random.random()*360
 
@@ -37,7 +37,7 @@ class Environment:
     def info(self):
         print("World size: " + str(self.x) + "x" + str(self.y) + ".")
         print("Node location: " + str(self.node))
-        print("Home location: " + str(self.home))
+        print("Agent location: " + str(self.agent))
         print("State: " + str(self.state()))
 
     # the world exports specific information about it's state
@@ -55,8 +55,8 @@ class Environment:
         # node direction angle
         # (calculated by using arc tangent)
         # https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
-        x = self.node[0] - self.home[0]
-        y = self.node[1] - self.home[1]
+        x = self.node[0] - self.agent[0]
+        y = self.node[1] - self.agent[1]
         d = math.degrees(math.atan2(y, x))
         if d < 0:
             d += 360  # let's keep things positive
@@ -65,7 +65,7 @@ class Environment:
         # (calculated with pythagorean theorem)
         # exact distance up to a limit, 0 if too far
         p_max = 100
-        p = ((self.node[0] - self.home[0])**2 + (self.node[1] - self.home[1])**2)**0.5
+        p = ((self.node[0] - self.agent[0])**2 + (self.node[1] - self.agent[1])**2)**0.5
         # return 0 if the distance is beyond the max viewable distance
         if p > p_max:
             p = 0
@@ -100,9 +100,41 @@ class Environment:
         self.orientation += rotate_amount
         self.orientation += random.random() * random_amount - 0.5  # center at 0
 
-        return DEFAULT_ACTION_REWARD
+        return self.DEFAULT_ACTION_REWARD
 
+    # turn right
+    # just like turning left, but the other way
+    def right(self):
+        rotate_amount = 10  # degrees
+        random_percent = 10  # percent of rotate_amount
+        random_amount = rotate_amount * random_percent / 100 # center at 0
 
+        self.orientation -= rotate_amount
+        self.orientation -= random.random() * random_amount - (random_amount / 2)  # center at 0
+
+        # be careful when subtracting past zero
+        if self.orientation < 0:
+            self.orientation += 360
+
+        return self.DEFAULT_ACTION_REWARD
+
+    # go forwards
+    # the agent's position in the world moves forward by a specified amount
+    # again, the distance is not exact but varies slightly
+    def forward(self):
+        forward_amount = 1  # units
+        random_percent = 10  # percent of forward_amount
+        random_amount = forward_amount * random_percent / 100 # center at 0
+
+        total_amount = forward_amount + random.random() * random_amount - (random_amount / 2)  # center at 0
+
+        # change the agent coordinates based on the direction the agent is facing
+        x_amount =  total_amount * math.cos(self.orientation)
+        y_amount = total_amount * math.sin(self.orientation)
+
+        self.agent = (self.agent[0] + x_amount, self.agent[1] + y_amount) # fix this tuple-y sadness
+
+        return self.DEFAULT_ACTION_REWARD
 
 
 
@@ -181,6 +213,11 @@ def main():
 
     # display information from agent's sensors
     agent.info()
+
+    world.forward()
+    world.info()
+    world.forward()
+    world.info()
 
 
 
