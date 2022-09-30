2284028e733808c642a97f03215036b977428e3f
Pearstopher <pearstopher@gmail.com>
2022-09-30 09:01:08 -0700

    added minimap circle to game display

--- game.py
+++ game.py
@@ -16,7 +16,15 @@ def game(environment):
     pygame.display.set_caption('Gathering Simulator')
 
     # Set up the drawing window
-    screen = pygame.display.set_mode([1000, 800])
+    screen = pygame.display.set_mode([1600, 800])
+    # 0-399 = info/buttons/tbd
+    # 400-1199 = map display
+    # 1200-1599 = minimap
+
+    # left, top, width, height
+    info = (0, 0, 400, 800)
+    map = (400, 0, 800, 800)
+    mini = (1200, 0, 400, 800)
 
     font = pygame.font.Font('freesansbold.ttf', 12)
     text = font.render(str(environment.state()), True, (0,255,0), (0,0,255))
@@ -39,9 +47,22 @@ def game(environment):
         text = font.render(str(environment.state()), True, (0, 255, 0), (0, 0, 255))
         screen.blit(text, textRect)
 
-        # Draw a solid blue circle in the center
-        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-        pygame.draw.rect(screen, (200,20,20), pygame.Rect(30, 30, 60, 60), 2)
+
+        # info section
+        pygame.draw.rect(screen, (200,20,20), pygame.Rect(info), 2)
+
+
+        # map section
+        pygame.draw.rect(screen, (200,20,20), pygame.Rect(map), 2)
+
+
+        # minimap section
+        pygame.draw.rect(screen, (200,20,20), pygame.Rect(mini), 2)
+
+        # circle: surface, color, center, radius, width
+        center_y = mini[2]//2  # circle center location (y) based on width of section
+        center_x = mini[0] + center_y  # circle center (x) is starting point plus half width
+        pygame.draw.circle(screen, (0, 0, 255), (center_x, center_y), center_y - 10)
 
         # Flip the display
         pygame.display.flip()
