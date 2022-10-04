5c64512be964e881983e3325998b11d93fd884de
Pearstopher <pearstopher@gmail.com>
2022-10-04 11:30:07 -0700

    pygame display is optional

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from agent import Agent
 from environment import Environment
 from graph import Graph
 
+# if threading is allowed,
+# a pygame window will show live visual progress *during* training
+# and a handful of visual examples will be shown *after* tranining
+THREAD = False
+# if graphing is allowed,
+# a plot of rewards over time will be shown *after* training
+
 
 def main():
     print("CS410 Reinforcement Learning Project")
@@ -29,45 +36,46 @@ def main():
     agent.info()
 
 
+    if THREAD:
+        # create a thread for the game
+        # ideally I will be able to cap the framerate of pygame without slowing the actual computation
+        import threading
+        lock = threading.Lock()
 
-    # create a thread for the game
-    # ideally I will be able to cap the framerate of pygame without slowing the actual computation
-    import threading
-    lock = threading.Lock()
+        # you can safely transfer stuff between threads with locks or queues if you wanna
+        # from queue import Queue
+        # q = Queue()
+        thr = threading.Thread(target=game, args=(world,lock))
+        thr.start()
 
-    # you can safely transfer stuff between threads with locks or queues if you wanna
-    # from queue import Queue
-    # q = Queue()
-    thr = threading.Thread(target=game, args=(world,lock))
-    thr.start()
+        import time  # sleep to slow down for visual display
 
-    import time # sleep to slow down for visual display
-
-    # it seems pygame just needs some time to start before we start goin crazy up in here
-    time.sleep(1)
+        # it seems pygame just needs some time to start before we start goin crazy up in here
+        time.sleep(1)
 
     # while the game interface is running, go ahead and let the agent run
     # the interface is purely a visual representation of what is going on behind the scenes
     # (although it currently can't be disabled)
     #
-    episodes = 100
+    episodes = 2000
     e = 0
     rewards = [0 for _ in range(episodes)]
-    while thr.is_alive() and e < episodes:
+    while (not THREAD or thr.is_alive()) and e < episodes:
 
         # let the agent train until it reaches the max number of episodes
         # (or the pygame window is closed)
-        lock.acquire()
+        if THREAD:
+            lock.acquire()
         reward = agent.episode()
         rewards[e] = reward
-        lock.release()
+        if THREAD:
+            lock.release()
         print("Episode:", e, "Total Reward:", reward)
         e += 1
         # time.sleep(0.001)
 
-
-
-    thr.join()  # Will wait till game is done
+    if THREAD:
+        thr.join()  # Will wait till game is done
 
     # initialize the visual graph
     graph = Graph(y_lim=max(rewards)+1000)
